4069f2a38d7cde289b7987cf1df2e4f05c77e2d8
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-03-03 11:37:31 +0100

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://opentdb.com/api.php?amount=20")
+      .then(res => setQuestions(res.data.results));
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <>
+      <h1>Quizz App</h1>
+      { questions.map( question => (
+        <Card key={index} question={question} />
+      ) ) }
+    </>
+  );
 }
 
-export default App
+export default App;
