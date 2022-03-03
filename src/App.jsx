922b6d3b62f81ae2ee30479ddc352e4d1eb41da2
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-03-03 15:23:01 +0100

    all card

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,33 @@ import Card from "./components/Card";
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [maxQuestion, setMaxQuestion] = useState(20);
+  const [goodAnswerCount, setGoodAnswerCount] = useState(0);
+  const [answerCount, setAnswerCount] = useState(0);
 
   useEffect(() => {
     axios
-      .get("https://opentdb.com/api.php?amount=20")
+      .get(`https://opentdb.com/api.php?amount=${maxQuestion}`)
       .then(res => setQuestions(res.data.results));
-  }, []);
+  }, [maxQuestion]);
 
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Quizz App</h1>
-      { questions.slice(0, 1).map( (question, index) => (
-        <Card key={index} question={question} />
-      ) ) }
+      <form action="" className="questionNumber">
+        <input type="number" id="number" defaultValue="20" aria-label="Entre le nombre de question désiré" onChange={ (e) => setMaxQuestion(e.target.value) } />
+      </form>
+      {questions.map((question, index) => (
+        <Card
+          key={index}
+          question={question}
+          setAnswerCount={setAnswerCount}
+          setGoodAnswerCount={setGoodAnswerCount}
+          answerCount={answerCount}
+          goodAnswerCount={goodAnswerCount}
+          maxQuestion={maxQuestion}
+        />
+      ))}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,54 @@
 import { useEffect, useState } from "react";
 
-const Card = ({ question }) => {
+const Card = (props) => {
   const [answerd, setAnswerd] = useState([]);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  const [isCorrect, setIsCorrect] = useState(null);
 
   useEffect(() => {
-    setAnswerd(question.incorrect_answers);
-    setAnswerd([...answerd, question.correct_answer]);
-    setAnswerd([...new Set(answerd)]);
-  }, []);
+    let badAnswer = props.question.incorrect_answers;
+    let goodAnswer = props.question.correct_answer;
+    setCorrectAnswer(props.question.correct_answer);
+    let allAnswer = [...badAnswer, goodAnswer];
+    setAnswerd(allAnswer);
+  }, [props.question]);
+
+  const handleClick = e => {
+    console.log(e.target.innerHTML);
+    if (!isAnswered) {
+      setIsAnswered(true);
+      props.setAnswerCount(props.answerCount + 1);
+
+      if (e.target.innerHTML === correctAnswer) {
+        setIsCorrect(true);
+        props.setGoodAnswerCount(props.goodAnswerCount + 1);
+      } else {
+        setIsCorrect(false);
+      }
+    }
+  };
 
   return (
     <div className='card-container'>
       <div className='hero-banner'>
-        <div className='good-answer'>⭕️ 0 / 9</div>
+        <div className='good-answer'>⭕️ { props.goodAnswerCount + ' / ' + props.answerCount }</div>
         <div className='title'>Average Level</div>
-        <div className='total-question'> 9 / 20 ❓</div>
+        <div className='total-question'> { props.answerCount + ' / ' + props.maxQuestion } ❓</div>
       </div>
       <div className='titre-container'>
-        <h2>{question.question}</h2>
+        <h2>{props.question.question}</h2>
 
-        <div className='question'>
+        <div className={isAnswered ? "question isAnswered" : "question"}>
           {answerd.map((answer, index) => (
-            <div key={index} className='answer'>
-              <p>{answer}</p>
+            <div
+              key={index}
+              className={
+                isCorrect ? "answer goodAnswered" : "answer badAnswered"
+              }
+              onClick={handleClick}
+            >
+              {answer}
             </div>
           ))}
         </div>
